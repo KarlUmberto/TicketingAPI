b1e2191b7c79ba07fdbc7a76d3962b01fbf3b5c5
Karl <karl.pongolini@gmail.com>
2023-09-21 11:28:39 +0300

    Add events[#186070768]

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const swaggerDocument = require("./docs/swagger.json");
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+app.get("/events", (req, res) => {
+    res.send([
+        {id: 1, name: "VOLT presents: BOU"}
+        {id: 1, name: "Alchemist"}
+    ])
+})
+
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`);
 })
