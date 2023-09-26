32e5385b323431366bd2815a0a4608c9330b0c2c
RemusLuht <remusluht2005@gmail.com>
2023-09-26 15:49:29 +0300

    Add venue data

---
+++ venues/data.js
@@ -0,0 +1,14 @@
+let data = [
+    { id: 1, name: "D3", location: "Telliskivi 62/2", capacity: 1500 },
+    { id: 2, name: "Lauluväljak", location: "Narva mnt 95", capacity: 75000 },
+    { id: 3, name: "Club Hollywood", location: "Vana-Posti 8", capacity: 1400 },
+    { id: 4, name: "Studio", location: "Sauna 1", capacity: 600 },
+    { id: 5, name: "Helitehas", location: "Madara 22a", capacity: 700 },
+    { id: 6, name: "Unibet Arena", location: "Paldiski mnt 104b", capacity: 17500 },
+    { id: 7, name: "Cathouse", location: "Tartu maantee 17", capacity: 800 },
+    { id: 8, name: "Alexela Concert Hall", location: " Estonia pst 9", capacity: 1800 }
+]
+
+exports.getAll = () => {
+    return data.map(g => { return { "id": g.id, "name": g.name, "location": g.location, "capacity": g.capacity}})
+}
